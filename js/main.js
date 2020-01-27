b90424f864c4f97dbf13f2582ad57b4de37053bf
jeyson <jeysonrgxd@gmail.com>
2020-01-27 00:47:53 -0500

    portafolio -50%

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const grid = new Muuri(".grid",{
+   layout:{
+      rounding:false
+   }
+});
+
+window.addEventListener("load", ()=>{
+   document.querySelector(".grid").classList.add("imagenes-cargadas")
+})
