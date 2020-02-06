8eac36bdf5557b9b06909d47b16103a5429a1888
jeyson <jeysonrgxd@gmail.com>
2020-02-06 18:36:31 -0500

    ultimo push

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 const grid = new Muuri(".grid",{
+   // para poder nosotros redondear y utilizar porcentaje revisar
+   // https://github.com/haltu/muuri#layout-
    layout:{
       rounding:false
    }
 });
 
-window.addEventListener("load", ()=>{
+window.addEventListener("load", ( )=>{
+   // para que refresque nuestro layout
+   grid.refreshItems().layout();
    document.querySelector(".grid").classList.add("imagenes-cargadas")
 })
