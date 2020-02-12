950b64dd65086230ea1dc9940bee1e746f0f5cbc
jeyson <jeysonrgxd@gmail.com>
2020-02-12 01:27:40 -0500

    creacion modal-img

---
+++ main.js
@@ -0,0 +1,29 @@
+import modalScript from './js/modal.js'
+
+const ipad = window.matchMedia('screen and (max-width: 767px)')
+const menu = document.querySelector('.menu')
+const burgerButton = document.querySelector('#burger-menu')
+const modal_img = document.querySelector(".modal-project")
+const modal_cont_img = modal_img.querySelector(".modal-cont-img")
+const loader_modal = modal_img.querySelector(".lds-ring")
+
+function validation(event) {
+   if (event.matches) {
+      burgerButton.addEventListener('click', hideShow)
+   } else {
+      burgerButton.removeEventListener('click', hideShow)
+   }
+}
+
+function hideShow() {
+   if (menu.classList.contains('is-active')) {
+      menu.classList.remove('is-active');
+   } else {
+      menu.classList.add('is-active');
+   }
+}
+
+
+modalScript(modal_img, loader_modal, modal_cont_img)
+ipad.addListener(validation)
+validation(ipad);
